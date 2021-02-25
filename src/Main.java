30486ba0919e589484aec9bbe5d75a7522e766fe
Eric Eugenio de Haro <eric.eugenio@students.salle.url.edu>
2021-02-25 16:45:41 +0100

    Data reading has been modified

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,5 @@
-import java.io.IOException;
-
 public class Main {
     public static void main(String[] args) {
-        DataReader reader = new DataReader();
 
-        try {
-            reader.parseData("graphXXS.paed");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        Graph graph = new Graph(reader.getEdgeList(), reader.getVertexList());
-        graph.initGraph();
-
-        GraphTraversal DFS = new GraphTraversal(reader.getVertexList().size(), graph.getMapping());
-        DFS.DFS(graph, reader.getVertexList().get(0));
-
-        //System.out.print(graph.toString());
     }
 }

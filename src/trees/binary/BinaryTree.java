b6e60feedb6927588181c9d46958a59ef1b79722
Lucas Azcue Abello <lucas.azcue@students.salle.url.edu>
2021-03-31 23:38:11 +0200

    Remove node from binary tree

--- src/trees/binary/BinaryTree.java
+++ src/trees/binary/BinaryTree.java
@@ -36,4 +36,58 @@ public class BinaryTree {
         }
     }
 
+    public void removeNode(String nodeName){
+            //we will have to find the node we want to delete
+        BinaryNode node = null; //Temporary representation of our node
+        BinaryNode auxNode; //Node that allows us to only affect the parent when it is not the root
+        
+        //Adapt the find closest value without affecting the parent
+        if(node.getParent() == null){
+            root = childConcatenance(node);
+            root.setParent(null);
+        }
+        else{
+            auxNode = childConcatenance(node);
+            setChild(node, auxNode);
+        }
+        
+    }
+
+    private BinaryNode childConcatenance(BinaryNode node){
+        BinaryNode auxNode;
+
+        if (node.getLeftChild() == null && node.getRightChild() == null) {
+            return null;
+        }
+        if (node.getRightChild() == null) {
+            auxNode = findClosestValueLeft(node.getLeftChild());
+            setChild(auxNode, auxNode.getLeftChild());
+        }
+        else{
+            auxNode = findClosestValueRight(node.getRightChild());
+            setChild(auxNode, auxNode.getRightChild());
+        }
+        auxNode.setRightChild(node.getRightChild());
+        auxNode.setLeftChild(node.getLeftChild());
+        
+        return auxNode;
+    }
+
+    private BinaryNode findClosestValueRight(BinaryNode root) {
+        return root.getLeftChild() == null ? root : findClosestValueRight(root.getLeftChild());
+    }
+    private BinaryNode findClosestValueLeft(BinaryNode root) {
+        return root.getRightChild() == null ? root : findClosestValueLeft(root.getRightChild());
+    }
+
+    private void setChild(BinaryNode root, BinaryNode node){
+        if(root.getParent().getRightChild() == root){
+            root.getParent().setRightChild(node);
+        }
+        else{
+            root.getParent().setLeftChild(node);
+        }
+        node.setParent(root.getParent());
+    }
+
 }

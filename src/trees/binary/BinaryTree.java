7b47024f91b8a91a4c0fa21422de6a270d5409a6
Lucas Azcue Abello <lucas.azcue@students.salle.url.edu>
2021-04-09 00:18:29 +0200

    Add balance method to the BinaryTree class

--- src/trees/binary/BinaryTree.java
+++ src/trees/binary/BinaryTree.java
@@ -1,5 +1,7 @@
 package trees.binary;
 
+import static java.lang.StrictMath.abs;
+
 public class BinaryTree {
 
     private BinaryNode root;
@@ -88,6 +90,83 @@ public class BinaryTree {
             root.getParent().setLeftChild(node);
         }
         node.setParent(root.getParent());
+    }
+
+    private void balance(BinaryNode newNode){
+        int bf = getBalanceFactor(newNode.getParent());
+        int bfParent = getBalanceFactor(newNode.getParent().getParent());
+
+        if(bf == 1 && bfParent == -2){
+            leftRotate(newNode.getParent().getParent());
+        }
+        if(bf == -1 && bfParent == 2){
+            rightRotate(newNode.getParent().getParent());
+        }
+        if(bfParent == 2){
+            leftRotate(newNode.getParent().getParent());
+        }
+        if(bfParent == -2){
+            rightRotate(newNode.getParent().getParent());
+        }
+
+    }
+
+    private void rightRotate(BinaryNode node){
+        BinaryNode B = node.getLeftChild();
+        BinaryNode P = node.getParent();
+
+        node.setLeftChild(B.getRightChild());
+        if(B.getRightChild() != null){
+            B.getRightChild().setParent(node);
+        }
+        B.setRightChild(node);
+        node.setParent(B);
+        B.setParent(P);
+        if(P != null){
+            if(P.getLeftChild() == node){
+                P.setLeftChild(B);
+            }
+            else{
+                P.setRightChild(B);
+            }
+        }
+    }
+
+    private void leftRotate(BinaryNode node){
+        BinaryNode B = node.getRightChild();
+        BinaryNode P = node.getParent();
+
+        node.setRightChild(B.getLeftChild());
+        if(B.getLeftChild() != null){
+            B.getLeftChild().setParent(node);
+        }
+        B.setLeftChild(node);
+        node.setParent(B);
+        B.setParent(P);
+        if(P != null){
+            if(P.getRightChild() == node){
+                P.setRightChild(B);
+            }
+            else{
+                P.setLeftChild(B);
+            }
+        }
+    }
+
+    private int getBalanceFactor(BinaryNode node){
+        return getNodeHeight(node.getRightChild()) - getNodeHeight(node.getLeftChild());
+    }
+
+    private int getNodeHeight(BinaryNode node){
+        int leftHeight, rightHeight;
+
+        if(node.getRightChild() == null && node.getLeftChild() == null){
+            return 0;
+        }
+        leftHeight = getNodeHeight(node.getLeftChild());
+        rightHeight = getNodeHeight(node.getRightChild());
+
+        return Math.max(leftHeight, rightHeight) + 1;
     }
 
 }
